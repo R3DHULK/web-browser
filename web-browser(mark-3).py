5969730ff082bb1e8a2590d089bbdabbdce60d55
Sumalya Chatterjee <105884682+R3DHULK@users.noreply.github.com>
2022-12-03 01:35:18 +0530

    Delete web-browser(mark-3).py

--- web-browser(mark-3).py
+++
@@ -1,7 +0,0 @@
-import tkinterweb
-import tkinter as tk
-root = tk.Tk()
-frame = tkinterweb.HtmlFrame(root)
-frame.load_website('www.facebook.com')
-frame.pack(fill="both", expand=True)
-root.mainloop()
